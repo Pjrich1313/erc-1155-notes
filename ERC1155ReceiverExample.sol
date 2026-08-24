ab4efbeae9aa6c2213328075647441c79d3962dd
Pammy12 <129231067+Pjrich1313@users.noreply.github.com>
2026-08-23 19:46:14 -0500

    Update ERC1155ReceiverExample with ReentrancyGuard

--- ERC1155ReceiverExample.sol
+++ ERC1155ReceiverExample.sol
@@ -3,16 +3,17 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-contract ERC1155ReceiverExample is ERC165, IERC1155Receiver {
+contract ERC1155ReceiverExample is ERC165, IERC1155Receiver, ReentrancyGuard {
     function onERC1155Received(
         address,
         address,
         uint256,
         uint256,
         bytes calldata
-    ) external pure override returns (bytes4) {
-        return this.onERC1155Received.selector; // 0xf23a6e61
+    ) external override nonReentrant returns (bytes4) {
+        return this.onERC1155Received.selector;
     }
 
     function onERC1155BatchReceived(
@@ -21,8 +22,8 @@ contract ERC1155ReceiverExample is ERC165, IERC1155Receiver {
         uint256[] calldata,
         uint256[] calldata,
         bytes calldata
-    ) external pure override returns (bytes4) {
-        return this.onERC1155BatchReceived.selector; // 0xbc197c81
+    ) external override nonReentrant returns (bytes4) {
+        return this.onERC1155BatchReceived.selector;
     }
 
     function supportsInterface(bytes4 interfaceId)
